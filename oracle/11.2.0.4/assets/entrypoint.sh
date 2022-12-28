3753bc911c9dbe438d078dfb3d0ee42967e91b96
Jove-2761781 <96041381+Jove-2761781@users.noreply.github.com>
2022-12-28 10:33:05 +0800

    fix some problems.

--- oracle/11.2.0.4/assets/entrypoint.sh
+++ oracle/11.2.0.4/assets/entrypoint.sh
@@ -6,6 +6,14 @@ source /assets/colorecho
 
 spfile=$ORACLE_HOME/dbs/spfile$ORACLE_SID.ora
 
+modify_installRSP () {
+	sed -i "s|#ORACLE_HOSTNAME#|$HOSTNAME|" /assets/db_install.rsp
+	sed -i "s|#ORACLE_BASE#|$ORACLE_BASE|" /assets/db_install.rsp
+	sed -i "s|#ORACLE_HOME#|$ORACLE_HOME|" /assets/db_install.rsp
+	sed -i "s|#ORACLE_INVENTORY#|$ORACLE_INVENTORY|" /assets/db_install.rsp
+	echo_yellow "File db_install.rsp modified."
+}
+
 modify_bashrc () {
 	sed -i "s|#ORACLE_SID#|$ORACLE_SID|" ~oracle/.bashrc
 	sed -i "s|#ORACLE_BASE#|$ORACLE_BASE|" ~oracle/.bashrc
@@ -16,16 +24,9 @@ modify_bashrc () {
 
 modify_dbcaRSP () {
 	sed -i "s|#ORACLE_SID#|$ORACLE_SID|" /assets/dbca.rsp
+	sed -i "s|#ORACLE_BASE#|$ORACLE_BASE|" /assets/dbca.rsp
 	sed -i "s|#CHARACTERSET#|$CHARACTERSET|" /assets/dbca.rsp
 	echo_yellow "File dbca.rsp modified."
-}
-
-modify_installRSP () {
-	sed -i "s|#ORACLE_HOSTNAME#|$HOSTNAME|" /assets/db_install.rsp
-	sed -i "s|#ORACLE_BASE#|$ORACLE_BASE|" /assets/db_install.rsp
-	sed -i "s|#ORACLE_HOME#|$ORACLE_HOME|" /assets/db_install.rsp
-	sed -i "s|#ORACLE_INVENTORY#|$ORACLE_INVENTORY|" /assets/db_install.rsp
-	echo_yellow "File db_install.rsp modified."
 }
 
 if [ ! -f "/etc/oratab" ]; then
@@ -41,4 +42,4 @@ if [ ! -f $spfile ]; then
 	su oracle -c "/assets/createDB.sh"
 fi
 
-su oracle -c "/assets/runOracleDB.sh"
+su oracle -c "/assets/runOracle.sh"
